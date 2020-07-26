c94600be296da63c6378063021faa7d18a490d98
Carmelo Giuliano <carmelo.giuliano1@gmail.com>
2020-07-26 23:45:21 +1000

    Separate youtube code into youtube.py

---
+++ src/ytm-spotify.py
@@ -0,0 +1,10 @@
+import youtube
+
+def main():
+    songList = youtube.getSongList('PL0m_RB_7CwlAU66D3XDZpVquBgt7psmoi')
+    for song in songList:
+        print(song)
+
+
+if __name__ == "__main__":
+    main()
